b272f32ce28cbc8e19007c707e2959bb4354eeae
brandon <bberning.web@gmail.com>
2021-08-11 10:06:52 -0400

    added changes for show work link

--- src/Pages/Home/comps/Hero.js
+++ src/Pages/Home/comps/Hero.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 import BgAnimation from '../../../BackgroundAnimation/BackgroundAnimation';
 import './Hero.scss'
 
@@ -16,9 +17,9 @@ const Hero = () => {
                 <p className="hero-text">
                     I would say my love for creating simple, efficient <br/>and beautiful components is what drives me to <br/>continue coding.
                 </p>
-                <div className="btn learn-more"
-                    onClick={()=> window.location = '/projects'}>
-                    View my work
+
+                <div className="btn learn-more">
+                    <Link to="/projects">View my work</Link>
                 </div>
             </div>
             <div className="right-side">
